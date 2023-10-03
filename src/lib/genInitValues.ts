205669308951bfd6eb2715b6adacbb19307d3e62
javi-diaz47 <javiereduardo300@gmail.com>
2023-10-03 15:07:09 -0500

    feat: add generate initial values

---
+++ src/lib/genInitValues.ts
@@ -0,0 +1,24 @@
+import type { Mixed } from '../types';
+import { PRIMES } from './primes';
+
+// Takes the value of m, and generate other input values
+// m must be prime
+export const genInitValues = (m: number): Mixed => {
+	const i = PRIMES.findIndex((n) => n === m);
+
+	// a and c must be less than m
+	const randPrime = () => PRIMES[Math.round((i - 1) * Math.random())];
+
+	const x = randPrime();
+	const a = randPrime();
+	const c = randPrime();
+
+	console.log({ x, a, c });
+
+	return {
+		x,
+		a,
+		c,
+		m
+	};
+};
---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+export interface Mixed {
+	x: number;
+	a: number;
+	c: number;
+	m: number;
+}
+
+export interface GeneratedMixed {
+	areUniform: boolean;
+	r: number[];
+}
+
+export interface Normalize {
+	r: number[];
+	m: number;
+}
