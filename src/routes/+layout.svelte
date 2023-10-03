ad4d3290648c2d8344e2e405a88880ccab2c65cd
javi-diaz47 <javiereduardo300@gmail.com>
2023-10-03 14:17:09 -0500

    feat: project setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Symulation</title>
+</svelte:head>
+
+<div class="bg-slate-800 h-screen text-white">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<main class="grid gap-4 p-4">
+	<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
